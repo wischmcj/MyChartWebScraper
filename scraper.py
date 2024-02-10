6de6114179abaa905209863d3aea037410e1ea1c
Collin <cjwischmeyer@gmail.com>
2024-02-10 13:11:42 -0500

    getting auth scraped

--- scraper.py
+++ scraper.py
@@ -1,24 +1,76 @@
 import requests
-import pandas as pd 
-import numpy as np
+# import numpy as np
 #!pip install selenium
-from selenium import webdriver
+# from selenium import webdriver
 
+from bs4 import BeautifulSoup 
 
-URL = "http://testphp.vulnweb.com/userinfo.php" 
+URL = "https://mychart.emoryhealthcare.org/mychart-prd/Authentication/Login/" 
  
+post_url  = "https://mychart.emoryhealthcare.org/MyChart-prd/Authentication/Login/DoLogin"
 payload = { 
 	"uname": "cwischmeye", 
 	"pass": "" 
 } 
-s = requests.session() 
-response = s.post(URL, data=payload) 
-print(response.status_code) # If the request went Ok we usually get a 200 status. 
+#  __RequestVerificationToken=OGvWMlwWIXHe8ONRHpH1UoSNGPoGDvl2KDytJCnogPo8_kvhQKwM-j4ZdhXl882x1ou3SiykuRyCpS65QhrqM2dphJ01&DeviceId=WEB%2C276a0198b48a4741b8d6b2605db47678%2C1QeY2eV4%2BnFGmxvY5KaPAkBfr5BA%2FNN6H9Kdku6KeZM%3D&postLoginUrl=&LoginInfo=%7B%22Type%22%3A%22StandardLogin%22%2C%22Credentials%22%3A%7B%22Username%22%3A%22Q3dpc2NobWV5%22%2C%22Password%22%3A%22TXlDaGFydHB3MzMh%22%7D%7D
+# __RequestVerificationToken=OGvWMlwWIXHe8ONRHpH1UoSNGPoGDvl2KDytJCnogPo8_kvhQKwM-j4ZdhXl882x1ou3SiykuRyCpS65QhrqM2dphJ01
+# DeviceId=WEB,276a0198b48a4741b8d6b2605db47678,1QeY2eV4+nFGmxvY5KaPAkBfr5BA/NN6H9Kdku6KeZM=
+# postLoginUrl
+
+# HTTP/1.1 302 Found
+# Cache-Control: no-cache, no-store, must-revalidate
+# Pragma: no-cache
+# Content-Type: text/html; charset=utf-8
+# Expires: -1
+# Location: /MyChart-prd/Authentication/Login?error=usernameloginfailed
+# Content-Security-Policy-Report-Only: default-src 'self';base-uri 'self';frame-ancestors 'self';frame-src https://* 'self' epichttp:;script-src 'nonce-8102f7b055a8490ab9326cda5a2d8088' https://mychart.eushc.org 'self';img-src https://* 'self' blob: data:;style-src https://mychart.eushc.org 'self' 'unsafe-inline';form-action 'self';media-src https://* 'self';
+# Pics-Label: (PICS-1.1 "http://www.icra.org/pics/vocabularyv03/" l on "2010.05.31T16:34-0400" exp "2100.12.31T12:00-0400" r (n 0 s 0 v 0 l 0 oa 0 ob 0 oc 0 od 0 oe 0 of 0 og 0 oh 0 c 0))
+# X-Content-Type-Options: nosniff
+# Strict-Transport-Security: max-age=31536000
+# Date: Sat, 10 Feb 2024 16:17:36 GMT
+# Content-Length: 176
+
+login_info = {"Type":"StandardLogin","Credentials":{"Username":"Q3dpc2NobWV5","Password":"TXlDaGFydHB3MzMh"}}
+Device_id = "WEB,276a0198b48a4741b8d6b2605db47678,1QeY2eV4+nFGmxvY5KaPAkBfr5BA/NN6H9Kdku6KeZM="
+login_info = '{"Type":"StandardLogin","Credentials":{"Username":"Q3dpc2NobWV5ZQ==","Password":"TXlDaGFydHB3MzMh"}}'
+medication_url = 'https://mychart.emoryhealthcare.org/MyChart-prd/inside.asp?mode=itinerary&sch=66878'
+itinerary_url = 'https://mychart.emoryhealthcare.org/MyChart-prd/inside.asp?mode=itinerary'
+
+given_Day_meds = 'https://mychart.emoryhealthcare.org/MyChart-prd/inside.asp?mode=itinerary&tod=1&cat=999&dat=66878'
+with requests.session() as auth_session: 
+    req = auth_session.get(URL).text 
+    html = BeautifulSoup(req,"html.parser") 
+    token = html.find("input", {"name": "__RequestVerificationToken"}).attrs["value"] 
+    payload = { "__RequestVerificationToken": token, "DeviceId": Device_id, "postLoginUrl": "", "LoginInfo": login_info}
+    req = auth_session.post(post_url,data=payload)
+    r = auth_session.get(medication_url) 
+    soup = BeautifulSoup (r.content, "html.parser") 
+    med_page = auth_session.get(given_Day_meds)
+    med_parser = BeautifulSoup(med_page.content, "html.parser")
+    med_elements = med_parser.find_all("td")
+
+	# usernameDiv = soup.find("span", class_="p-nickname vcard-username d-block") 
+    breakpoint()
+    # html = BeautifulSoup(req,"html.parser")
+    breakpoint()
+    auth_session.close()
+    html.find()
+
+
+# with requests.session() as scrape_sesh: 
+# 	req = auth_session.get(post_url,payload=payload)
+# 	html = BeautifulSoup(req,"html.parser") 
+
+# LoginInfo={"Type":"StandardLogin","Credentials":{"Username":"Q3dpc2NobWV5","Password":"TXlDaGFydHB3MzMh"}}
+# s = requests.session() 
+# response = s.post(URL, data=payload) 
+# print(response.status_code) # If the request went Ok we usually get a 200 status. 
  
-from bs4 import BeautifulSoup 
-soup = BeautifulSoup(response.content, "html.parser") 
-protected_content = soup.find(attrs={"id": "pageName"}).text 
-print(protected_content)
+# soup = BeautifulSoup(response.content, "html.parser") 
+# protected_content = soup.find(attrs={"id": "pageName"}).text 
+# print(protected_content)
+
+
 
 def prescription_list(row):     
     """ This function returns a list of all the prescription       
@@ -76,19 +128,19 @@ def nhs_details(drug):
 
 
 # <input name="__RequestVerificationToken" 
-type="hidden" 
-value="OGvWMlwWIXHe8ONRHpH1UoSNGPoGDvl2KDytJCnogPo8_kvhQKwM-j4ZdhXl882x1ou3SiykuRyCpS65QhrqM2dphJ01" 
-autocomplete="off">
+# type="hidden" 
+# value="OGvWMlwWIXHe8ONRHpH1UoSNGPoGDvl2KDytJCnogPo8_kvhQKwM-j4ZdhXl882x1ou3SiykuRyCpS65QhrqM2dphJ01" 
+# autocomplete="off">
 
-<form class="hidden" 
-action="/MyChart-prd/Authentication/Login/DoLogin" 
-autocomplete="off" id="actualLogin" 
-method="post" target="_top">
-<input name="__RequestVerificationToken"
-type="hidden" 
-value="OGvWMlwWIXHe8ONRHpH1UoSNGPoGDvl2KDytJCnogPo8_kvhQKwM-j4ZdhXl882x1ou3SiykuRyCpS65QhrqM2dphJ01" autocomplete="off">
-<div class="formcontents">
-</div>
-<input type="hidden" name="DeviceId" 
-value="WEB,276a0198b48a4741b8d6b2605db47678,1QeY2eV4+nFGmxvY5KaPAkBfr5BA/NN6H9Kdku6KeZM=">
-<input type="hidden" name="postLoginUrl" value=""></form>
+# <form class="hidden" 
+# action="/MyChart-prd/Authentication/Login/DoLogin" 
+# autocomplete="off" id="actualLogin" 
+# method="post" target="_top">
+# <input name="__RequestVerificationToken"
+# type="hidden" 
+# value="OGvWMlwWIXHe8ONRHpH1UoSNGPoGDvl2KDytJCnogPo8_kvhQKwM-j4ZdhXl882x1ou3SiykuRyCpS65QhrqM2dphJ01" autocomplete="off">
+# <div class="formcontents">
+# </div>
+# <input type="hidden" name="DeviceId" 
+# value="WEB,276a0198b48a4741b8d6b2605db47678,1QeY2eV4+nFGmxvY5KaPAkBfr5BA/NN6H9Kdku6KeZM=">
+# <input type="hidden" name="postLoginUrl" value=""></form>"td"
